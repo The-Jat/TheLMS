76f0c2c2e65e8598b4871ea0ac2467f334cbd1f2
MaK <manishkl543@gmail.com>
2023-09-03 20:55:23 +0530

    first commit

---
+++ storage/framework/views/aa8961ebd2902ad11233d9b90f32fdb2.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::illustrated-layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\manis\Downloads\traineasy-24aug23\codecanyon-19457888-traineasy-training-management-registration-system\app\resources\views/errors/404.blade.php ENDPATH**/ ?>
